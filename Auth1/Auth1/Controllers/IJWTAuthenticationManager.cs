839401c5d7632a6ace8c43a4293223f133449e2e
prachipanchal353 <101180023+prachipanchal353@users.noreply.github.com>
2022-10-28 02:00:09 -1200

    Add files via upload

---
+++ Auth1/Auth1/Controllers/IJWTAuthenticationManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Auth1.Controllers
+{
+    public class IJWTAuthenticationManager
+    {
+        internal object Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Auth1/Auth1/Controllers/NameController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auth1.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJWTAuthenticationManager jwtAuthenticationManager;
+
+        public NameController(IJWTAuthenticationManager jwtAuthenticationManager )
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<NameController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        [AllowAnonymous]
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jwtAuthenticationManager.Authenticate
+                (userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+            return Ok();
+        }
+
+        
+    }
+}
